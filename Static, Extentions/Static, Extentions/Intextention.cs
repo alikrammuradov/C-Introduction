5e85d5953599340fedd4074c6c5ae112d769a348
Alikram Muradov <alikrammuradov>
2025-10-21 06:40:46 +0400

    yeni

---
+++ Static, Extentions/Static, Extentions/Intextention.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyExtensions
+{
+    public static class IntExtensions
+    {
+        public static double Power(this int number, int exponent)
+        {
+            if (exponent == 0)
+                return 1;
+
+            double result = 1;
+            double baseValue = number;
+            int absExponent = Math.Abs(exponent);
+
+            for (int i = 0; i < absExponent; i++)
+            {
+                result *= baseValue;
+            }
+
+            if (exponent < 0)
+            {
+                if (number == 0)
+                    throw new DivideByZeroException("0 mənfi qüvvətə qaldırıla bilməz.");
+                result = 1 / result;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Static, Extentions/Static, Extentions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MyExtensions; // Extension methodu işlətmək üçün namespace əlavə olunur
+
+namespace ExtensionMethodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ededi daxil et: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Qüvveti daxil et: ");
+            int power = int.Parse(Console.ReadLine());
+
+            double result = number.Power(power);
+            Console.WriteLine($"{number}^{power} = {result}");
+        }
+    }
+}
