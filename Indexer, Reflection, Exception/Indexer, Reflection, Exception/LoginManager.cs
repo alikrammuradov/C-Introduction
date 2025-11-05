92ba2c2bc65734012fa05e4fd89e2d6420e2c0b2
Alikram Muradov <alikrammuradov>
2025-11-05 06:44:41 +0400

    Exception

---
+++ Indexer, Reflection, Exception/Indexer, Reflection, Exception/LoginManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer__Reflection__Exception
+{
+    public class LoginManager
+    {
+        private List<User> users = new List<User>();
+
+        public LoginManager()
+        {
+            // Test üçün bir neçə istifadəçi əlavə edək
+            users.Add(new User("alex", "12345"));
+            users.Add(new User("admin", "admin123"));
+        }
+
+        public void Login(string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                throw new LoginException("İstifadəçi adı boş ola bilməz!");
+
+            if (string.IsNullOrWhiteSpace(password))
+                throw new LoginException("Şifrə boş ola bilməz!");
+
+            User found = users.Find(u => u.Username == username);
+
+            if (found == null)
+                throw new LoginException("Bu adda istifadəçi mövcud deyil!");
+
+            if (found.Password != password)
+                throw new LoginException("Şifrə yanlışdır!");
+
+            Console.WriteLine("Uğurla daxil oldunuz!");
+        }
+    }
+}
---
+++ Indexer, Reflection, Exception/Indexer, Reflection, Exception/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace Indexer__Reflection__Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LoginManager login = new LoginManager();
+
+            try
+            {
+                Console.Write("İstifadəçi adını daxil edin: ");
+                string username = Console.ReadLine();
+
+                Console.Write("Şifrəni daxil edin: ");
+                string password = Console.ReadLine();
+
+                login.Login(username, password);
+            }
+            catch (LoginException ex)
+            {
+                Console.WriteLine($"Xəta: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Gözlənilməyən xəta: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Proqram sonlandı.");
+            }
+
+            //    //try
+            //    //{
+            //    //    int[] nums = new int[10];
+            //    //    nums[10] = 100;
+
+            //    //    int a = 5;
+            //    //    int b = 0;
+
+            //    //    Console.WriteLine(a / b);
+            //    //}
+            //    //catch (DivideByZeroException ex)
+            //    //{
+            //    //     Console.WriteLine(ex.Message);
+            //    //}
+            //    //catch(IndexOutOfRangeException ex)
+            //    //{
+            //    //    Console.WriteLine(ex.Message); ;
+            //    //}
+            //    //finally
+            //    //{
+            //    //    Console.WriteLine("Duzelis et");
+            //    //}
+
+            //    try
+            //    {
+
+            //        //int a = 7;
+            //        //int c = 0;
+            //        //if (c == 0)
+            //        //{
+            //        //    throw new DividedException("0-a bolunme yoxdur!");
+            //        //}
+            //        //else 
+            //        //{
+            //        //    Console.WriteLine(a / c);
+            //        //}
+            //        List<string> names = new() { "ilkin", "Muhamed", "Aysel" };
+            //        var result = FindName(names, "Aysel");
+
+            //        if (result)
+            //        {
+            //            Console.WriteLine("tapildi");
+            //        }
+            //        else
+            //        {
+            //            throw new NotFoundedException("hele de axtarilir");
+            //        }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    }
+
+            //}
+            //public static bool FindName(List<string> names, string searchName)
+            //{
+            //    if (names.Contains(searchName))
+            //    {
+            //        return true;
+            //    }
+
+
+            //    return false;
+
+        }
+    }
+}
